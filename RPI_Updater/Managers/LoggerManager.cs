828087400594691319fe5610f65374b8533108ca
unknown <antoine.anxionnat@outlook.fr>
2020-12-22 00:10:41 +0100

    First commit

---
+++ RPI_Updater/Managers/LoggerManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPI_Updater.Managers
+{
+    public class LoggerManager
+    {
+
+    }
+}
---
+++ RPI_Updater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPI_Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
